1e715a6c100c0b88157ed6922b189d638a2f913b
Kit France <cjgfrance22@gmail.com>
2025-06-09 20:36:35 +0100

    feat: add Playwright testing

---
+++ tests/kits-dna.spec.js
@@ -0,0 +1,21 @@
+// @ts-check
+import { test, expect } from "@playwright/test";
+
+test("", async ({ page }) => {
+  await page.goto("/");
+  await expect(page).toHaveTitle(/Kit France/);
+  await page.goto("/");
+  await page.getByRole("button", { name: "About me" }).click();
+  await expect(page.getByRole("heading", { name: "About me" })).toBeVisible();
+  await page.getByRole("link", { name: "CF" }).click();
+  await page.getByRole("button", { name: "Product" }).click();
+  await expect(page.getByRole("article").filter({ hasText: "Business outcomes" })).toBeVisible();
+  await page.getByRole("link", { name: "Projects", exact: true }).click();
+  await expect(page.getByText("Jamstack kits-dna My personal")).toBeVisible();
+  await page.getByRole("link", { name: "Blog", exact: true }).click();
+  await expect(page.getByRole("heading", { name: "Blog" })).toBeVisible();
+  await page.getByTitle("Search").click();
+  await expect(page.getByRole("textbox", { name: "Search" })).toBeVisible();
+  await page.getByRole("link", { name: "Home" }).click();
+  await expect(page.getByRole("button", { name: "Dark/Light Mode" })).toBeVisible();
+});
